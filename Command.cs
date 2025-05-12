d7e960e9ba399436dc83ae0ea23031f82beb70e2
IIIAuPMa <kalash1605@mail.ru>
2025-05-13 01:39:27 +0700

    Add files via upload

---
+++ Command.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandSystem;
+using LabApi.Features.Wrappers;
+using MapGeneration;
+using static Unity.Collections.AllocatorManager;
+
+namespace ControllClassDRoom
+{
+
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    internal class Command : ICommand
+
+    {
+        public string[] Aliases => Array.Empty<string>();
+
+        public string Description => "Controlling system in Class-D Block <color=yellow> {help} {status} ";
+
+        string ICommand.Command => "dblock";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+
+            if (arguments.At(0) == "help")
+            {
+                response = "\nCOMMANDS: <color=red>LOCK <color=red>CLOSE <color=green>UNLOCK <color=green>OPEN";
+
+                return true;
+            }
+            if (arguments.At(0) == "status")
+            {
+                response = null;
+
+                foreach (Room room in Room.Get(RoomName.LczClassDSpawn))
+                {
+                    if (room.Name == RoomName.LczClassDSpawn)
+                    {
+                        foreach (var Cdoor in room.Doors.Take(14))
+                        {
+                            if (Cdoor.IsLocked == true)
+                            {
+
+                                response = "<color=red>CLOSED";
+
+                                return true;
+
+                            }
+                            if (Cdoor.IsLocked == false)
+                            {
+
+                                response = "<color=green>UNCLOSED";
+
+                                return true;
+
+                            }
+                        }
+
+                        return true;
+                    }
+
+                }
+
+
+                return true;
+            }
+
+            if (arguments.Count == 0)
+            {
+
+                response = "Set values for controlling a Class-d Block";
+
+                return false;
+
+            }
+            if (arguments.Count > 2)
+            {
+
+                response = "Please set a number";
+
+                return false;
+
+            }
+            if (arguments.Count > 3)
+            {
+
+                response = "Arguments can'not be three more";
+
+                return false;
+
+            }
+            if (arguments.At(0) == "unlock")
+            {
+
+                foreach (Room room in Room.Get(RoomName.LczClassDSpawn))
+                {
+                    response = "<color=green>All Class D Doors unlocked";
+                    if (room.Name == RoomName.LczClassDSpawn)
+                    {
+                        foreach (var Cdoor in room.Doors.Take(14))
+                        {
+                            Cdoor.Lock(Interactables.Interobjects.DoorUtils.DoorLockReason.Isolation, false);
+                        }
+
+                        return true;
+                    }
+
+                }
+
+            }
+            if (arguments.At(0) == "lock")
+            {
+
+                foreach (Room room in Room.Get(RoomName.LczClassDSpawn))
+                {
+                    response = "<color=red>All Class D Doors locked";
+                    if (room.Name == RoomName.LczClassDSpawn)
+                    {
+                        foreach (var Cdoor in room.Doors.Take(14))
+                        {
+                            Cdoor.Lock(Interactables.Interobjects.DoorUtils.DoorLockReason.Isolation, true);
+
+                            Cdoor.IsOpened = false;
+                        }
+                        return true;
+                    }
+                }
+            }
+            if (arguments.At(0) == "close")
+            {
+
+                foreach (Room room in Room.Get(RoomName.LczClassDSpawn))
+                {
+                    response = "<color=red>All Class D Doors closed";
+                    if (room.Name == RoomName.LczClassDSpawn)
+                    {
+                        foreach (var Cdoor in room.Doors.Take(14))
+                        {
+                            Cdoor.IsOpened = false;
+                        }
+                        return true;
+                    }
+                }
+
+            }
+            if (arguments.At(0) == "open")
+            {
+
+                foreach (Room room in Room.Get(RoomName.LczClassDSpawn))
+                {
+                    response = "<color=green>All Class D Doors opened";
+                    if (room.Name == RoomName.LczClassDSpawn)
+                    {
+                        foreach (var Cdoor in room.Doors.Take(14))
+                        {
+                            Cdoor.IsOpened = true;
+                        }
+                        return true;
+                    }
+                }
+
+            }
+
+            //------------------------------------
+
+            response = "Set values for controlling a Class - D Block";
+
+            return false;
+        }
+    }
+}
+
---
+++ Plugin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandSystem;
+using Interactables.Interobjects;
+using LabApi.Events;
+using LabApi.Features;
+using LabApi.Features.Wrappers;
+using LabApi.Loader.Features.Plugins;
+using MapGeneration;
+
+namespace ControllClassDRoom
+{
+    public class Plugin : LabApi.Loader.Features.Plugins.Plugin
+    {
+
+        
+
+        System.Random random = new System.Random();
+        public override string Name => "ControllPlugin";
+
+        public override string Description => "Controll or Lock ClassD Room or Doors";
+
+        public override string Author => "IIIAuPMa";
+
+        public override Version Version => new Version(1, 4, 1);
+
+        public override Version RequiredApiVersion => new Version(1, 4);
+
+        public override void Disable()
+        {
+            LabApi.Events.Handlers.ServerEvents.RoundStarted -= OnControll;
+        }
+
+        public override void Enable()
+        {
+            LabApi.Events.Handlers.ServerEvents.RoundStarted += OnControll;
+        }
+        public void OnControll()
+        {
+
+            List<Room> doors = new List<Room>();
+
+            foreach (Door Doors in Door.Get(FacilityZone.LightContainment))
+
+            foreach (Room room in Room.Get(RoomName.LczClassDSpawn)) 
+
+            foreach (Room MainCDRoom in Room.Get(FacilityZone.LightContainment))
+
+            foreach (Player player in Player.List)
+                        {
+                           
+                        if (room.Name == RoomName.LczClassDSpawn)
+                            {
+                                foreach (var Cdoor in room.Doors.Take(14)) 
+                                {
+                                    Cdoor.Lock(Interactables.Interobjects.DoorUtils.DoorLockReason.Isolation, true);
+                                }
+                            }
+
+                        }
+            }
+
+        }
+
+
+    }
