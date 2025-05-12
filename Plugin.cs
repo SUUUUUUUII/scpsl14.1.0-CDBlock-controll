8fb6c9fa80ee403ce2f4a3321df446677ebc043a
IIIAuPMa <kalash1605@mail.ru>
2025-05-12 21:50:44 +0300

    Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CommandSystem;
 using Interactables.Interobjects;
 using LabApi.Events;
@@ -15,20 +11,12 @@ namespace ControllClassDRoom
 {
     public class Plugin : LabApi.Loader.Features.Plugins.Plugin
     {
-
-        
-
         System.Random random = new System.Random();
         public override string Name => "ControllPlugin";
-
         public override string Description => "Controll or Lock ClassD Room or Doors";
-
         public override string Author => "IIIAuPMa";
-
         public override Version Version => new Version(1, 4, 1);
-
         public override Version RequiredApiVersion => new Version(1, 4);
-
         public override void Disable()
         {
             LabApi.Events.Handlers.ServerEvents.RoundStarted -= OnControll;
@@ -40,7 +28,6 @@ namespace ControllClassDRoom
         }
         public void OnControll()
         {
-
             List<Room> doors = new List<Room>();
 
             foreach (Door Doors in Door.Get(FacilityZone.LightContainment))
@@ -50,20 +37,19 @@ namespace ControllClassDRoom
             foreach (Room MainCDRoom in Room.Get(FacilityZone.LightContainment))
 
             foreach (Player player in Player.List)
-                        {
+                   {
                            
-                        if (room.Name == RoomName.LczClassDSpawn)
-                            {
-                                foreach (var Cdoor in room.Doors.Take(14)) 
-                                {
-                                    Cdoor.Lock(Interactables.Interobjects.DoorUtils.DoorLockReason.Isolation, true);
-                                }
-                            }
+                   if (room.Name == RoomName.LczClassDSpawn)
+                   {
+                   foreach (var Cdoor in room.Doors.Take(14)) 
+                   {
+                   
+                    Cdoor.Lock(Interactables.Interobjects.DoorUtils.DoorLockReason.Isolation, true);
+                    
+                   }
+                }
 
-                        }
+                }
             }
-
         }
-
-
     }
